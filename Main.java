49e5dcfeb5c27709f99e0117f4497dcc189c4480
TristanP <tristanpere@googlemail.com>
2023-03-07 17:16:37 +0000

    Update: Changed the game Start message.

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +8,10 @@ public class Main {
         String randomWord = wordArr.randomWord();
         Game game = new Game(randomWord);
         Scanner scanner = new Scanner(System.in);
-        System.out.println(game.getGuessedDashArr());
+        String newLine = System.getProperty("line.separator");
+        System.out.println(String.join(newLine,
+                "Welcome to Hangman. You have 8 lives to guess the correct letters in the word.",
+                game.getGuessedDashArr() ));
         while(game.getLives()>0 && game.getCorrectGuesses()!=0){
             System.out.println("Enter Guess:");
             String guess = scanner.nextLine();
